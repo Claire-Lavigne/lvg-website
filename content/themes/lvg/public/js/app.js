cb2824e8641cfefc919a7ef79c1677627b8ecb72
helio <helio-cruz@hotmail.com>
2019-10-15 22:16:53 +0200

    mis en page photographer

--- content/themes/lvg/public/js/app.js
+++ content/themes/lvg/public/js/app.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8722c243ce718a57e928";
+/******/ 	var hotCurrentHash = "e1eaf786e0de5a374476";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
