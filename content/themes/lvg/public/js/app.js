86386e0dca9c625946a63315cb50ee307716ab1e
Claire-Lavigne <lavigneclaire81@gmail.com>
2019-10-16 21:31:46 +0200

    contact adjusted with flexbox

--- content/themes/lvg/public/js/app.js
+++ content/themes/lvg/public/js/app.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4e57bf8230235f4a59c6";
+/******/ 	var hotCurrentHash = "136ed83aae2ce9c4e0bf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
