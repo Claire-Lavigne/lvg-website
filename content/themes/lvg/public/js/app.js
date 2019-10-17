f8331b14dda8f2d412d467126d5fd77baaed4de3
Claire-Lavigne <lavigneclaire81@gmail.com>
2019-10-17 09:32:07 +0200

    typography actualized

--- content/themes/lvg/public/js/app.js
+++ content/themes/lvg/public/js/app.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0c10bab7e2438025f643";
+/******/ 	var hotCurrentHash = "70916cc247980224a5df";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
