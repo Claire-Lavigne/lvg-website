13823ec787dedba32c2f660641b7e809c3a66d10
Claire-Lavigne <lavigneclaire81@gmail.com>
2019-10-24 10:28:10 +0200

    modifying infos wedding

--- content/themes/lvg/public/js/app.js
+++ content/themes/lvg/public/js/app.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f7290777e680c5eccd73";
+/******/ 	var hotCurrentHash = "4d4e41a7977995ef1c7e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
